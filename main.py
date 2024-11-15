c8c96e15fc319f39003e1675a759e443f4cac93a
Viktoria Dolgolevets <vikadolgolevec@gmail.com>
2024-11-15 20:23:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,37 +1,39 @@
 import json #подключение модуля
-a = 0
-stars = {"id": 0, "name":"", "constellation": "", "is_visible": "", "radius":0 }
-with open('stars.json', 'r') as file:
+a = 0 # пользователь вводит пункт
+stars = {"id": 0, "name":"", "constellation": "", "is_visible": "", "radius":0 } #для добавлния
+with open('stars.json', 'r') as file: # открывает файл и запись в data
     data = json.load(file)
-    while a!= 5: 
-        k = 0
-        print("1 - Вывести все записи\n2 - Вывести запись по полю\n3 - Добавить запись\n4 - Удалить запись по полю\n5 - Выйти из программы\n6 - Вывести запись по полю\n")
-        a = int(input("выберите действие: "))
-        if a == 1:
+    while a!= 5: #цикл повторяется пока не будет введен 5 
+        k = 0 #нахождение порядка записи 
+        print("1 - Вывести все записи\n2 - Вывести запись по полю\n3 - Добавить запись\n4 - Удалить запись по полю\n5 - Выйти из программы\n")
+        a = int(input("выберите действие: ")) #запрашивает действие
+        if a == 1: #выводит все 
             print(" ")
             for star in data:
                 print(f"{star["id"]}: {star["name"]}, название созвездия: {star["constellation"]}, радиус равен: {star["radius"]}, видимость: {'да' if star["is_visible"] else 'нет'}\n")
-        elif a == 2:
+        elif a == 2: #поле записи для пользователя
             inid = int(input("введите поле выводимой записи: "))
             for star in data:
                 k += 1
                 if inid == star["id"]:
                     print(f"{star["id"]}: {star["name"]}, название созвездия: {star["constellation"]}, радиус равен: {star["radius"]}, видимость: {'да' if star["is_visible"] else 'нет'}\n")
-        elif a == 3:
+        elif a == 3: #ввод данных
             stars["id"] = int(input("введите id: "))
             stars["name"] = int(input("введите название звезды: "))
             stars["constellation"] = int(input("введите название созвездия: "))
             stars["is_visible"] = (True if stars["constellation"] == 'да' else 'нет')
             stars["radius"] = int(input("введите радиус: "))
             data.append(stars)
-        elif a == 4:
+        elif a == 4: #нахождение id в файле и последующее удаление       
             inid = int(input("введите поле для удаления: "))
             for star in data:
                 if inid == star["id"]:
                     data.pop(k)
                 k += 1
-        elif a == 5:
+        elif a == 5: #завершение цикла
             print('завершение работы программы')
             break
+        else: #при вводе неправильного
+            print('введите правильное значение') 
 with open('stars.json', 'w', encoding='utf-8') as file:
     json.dump(data, file, indent=4, ensure_ascii=True)
