70c62268e6153caacfec9e56c7a34895feb8aace
Viktoria Dolgolevets <vikadolgolevec@gmail.com>
2024-12-09 13:32:40 +0300

    Update index.py

--- index.py
+++ index.py
@@ -1,44 +1,73 @@
-import json #подключение модуля
-a = 0 # пользователь вводит пункт
-stars = {"id": 0, "name":"", "constellation": "", "is_visible": "", "radius":0 } #для добавлния
-with open('stars.json', 'r', encoding='utf-8') as file: # открывает файл и запись в data
-    data = json.load(file)
-    while a!= 5: #цикл повторяется пока не будет введен 5 
-        k = 0 #нахождение порядка записи 
-        flage = True
-        print("1 - Вывести все записи\n2 - Вывести запись по полю\n3 - Добавить запись\n4 - Удалить запись по полю\n5 - Выйти из программы\n")
-        a = int(input("выберите действие: ")) #запрашивает действие
-        if a == 1: #выводит все 
-            print(" ")
-            for star in data:
-                print(f'{star["id"]}: {star["name"]}, название созвездия: {star["constellation"]}, радиус равен: {star["radius"]}, видимость: {"да" if star["is_visible"] else "нет"}\n')
-        elif a == 2: #поле записи для пользователя
-            inid = int(input("введите поле выводимой записи: "))
-            for star in data:
-                k += 1
-                if inid == star["id"]:
-                    print(f'{star["id"]}: {star["name"]}, название созвездия: {star["constellation"]}, радиус равен: {star["radius"]}, видимость: {"да" if star["is_visible"] else "нет"}\n')
-                    flage = False
-            print("нужная запись не найдена\n" if flage else "")        
-        elif a == 3: #ввод данных
-            stars["id"] = int(input("введите id: "))
-            stars["name"] = input("введите название звезды: ")
-            stars["constellation"] = input("введите название созвездия: ")
-            stars["is_visible"] = (True if stars["constellation"] == "да" else "нет")
-            stars["radius"] = int(input("введите радиус: "))
-            data.append(stars)
-        elif a == 4: #нахождение id в файле и последующее удаление       
-            inid = int(input("введите поле для удаления: "))
-            for star in data:
-                if inid == star["id"]:
-                    data.pop(k)
-                    flage = False  
-                k += 1
-            print("нужная запись не найдена\n" if flage else "") 
-        elif a == 5: #завершение цикла
-            print("завершение работы программы")
-            break
-        else: #при вводе неправильного
-            print("введите правильное значение") 
-with open('stars.json', 'w', encoding='utf-8') as file: #инф заноситься обратно в файл
-    json.dump(data, file, indent=4, ensure_ascii=True)
+import json
+def allrecords(data): #вывод всех записей
+    for stars in data: #переборка всех записей и их вывод
+        print(f'{stars["id"]}: {stars["name"]}, название созвездия: {stars["constellation"]}, радиус равен: {stars["radius"]}, видимость: {"да" if stars["is_visible"] else "нет"}\n')
+def record1(data): # вывод определенной записи
+    a = 0 #переменная для номера 
+    flag = True #флаг для нахождения записи
+    inpid = input("Введите поле выводимой записи: ")
+    if inpid.isdigit():
+        inpid = int(inpid)
+    else:
+        print("неправильный ввод\n")
+        return 0
+    for stars in data: #цикл с поиском записи
+        a += 1
+        if inpid == stars["id"]:
+            print(f'{stars["id"]}: {stars["name"]}, название созвездия: {stars["constellation"]}, радиус равен: {stars["radius"]}, видимость: {"да" if stars["is_visible"] else "нет"}\n')
+            flag = False
+    print("Запись не найдена\n" if flag else "")
+def newRecord(data): #создание записи
+    newStar= {"id":0, "name":"", "constellation":"", "is_visible":False,"radius":0} #множество для добавляемой записи
+    newStar["id"] = int(input("введите id: "))
+    newStar["name"] = input("введите название звезды: ")
+    newStar["constellation"] = input("введите название созвездия: ")
+    try:
+        newStar["is_visible"] = (True if newStar["constellation"] == "да" else "нет")
+    except:
+        print("неправильное значение\n")
+    newStar["radius"] = int(input("введите радиус: "))
+    data.append(newStar) # записывается в data
+def deleteRecord(data):#удаления записи
+    a = 0#переменная для номера записи
+    flag = True#флаг для нахождения записи
+    inpid = input("Введите поле записи для удвления: ")
+    if inpid.isdigit():
+        inpid = int(inpid)
+    else:
+        print("Некорректный ввод\n")
+        return 0
+    for stars in data: #цикл с поиском записи и удалением её из data
+        if inpid == stars["id"]:
+            data.pop(a)
+            flag = False
+        a += 1
+    print("Запись не найдена\n" if flag else "")
+def closingProg(data, num): #закрытие программы и перенос data в файл json
+    print(f'Завершение работы программы. Было выполнено {num} действий')
+    with open('stars.json', 'w', encoding='utf-8') as file: #инф записывается обратно в файл
+        json.dump(data, file, indent=4, ensure_ascii=True)
+    exit()
+def menu(data, num): #функция "меню"
+    print("1 - Вывести все записи\n2 - Вывести запись по полю\n3 - Добавить запись\n4 - Удалить запись по полю\n5 - Выйти из программы\n")
+    b = int(input("Выберите действие(введите число): ")) #запрашивает действие пользователя в переменную
+    if b == 1: #выводит все записи
+        allrecords(data)
+    elif b == 2:#выводит определенную запись введенную пользователем
+        record1(data)
+    elif b == 3: #ввод всех данных их запись и добав в data
+        newRecord(data)
+    elif b == 4: #находится id в файле и удаляет ее
+        deleteRecord(data)
+    elif b == 5: #завершается цикл
+        closingProg(data, num) 
+    else: #при вводе некорректного значения
+        print('Введите корректное значение')
+def main(): #главная функция
+    num = 0
+    with open('stars.json', 'r', encoding='utf-8') as file: #из json и записывается в data
+        data = json.load(file)
+        while True: #цикл повторяется постоянно
+            menu(data, num) #запускается меню
+            num += 1
+main() #запуск 
